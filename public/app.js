964b0cbe4a6f80039ae9c6d9b7e47a34ed37c05b
Noam <noamg@cox.net>
2016-03-23 21:00:32 -0400

    dummy

--- public/app.js
+++ public/app.js
@@ -3,8 +3,11 @@ var app = angular.module('myApp', []);
 app.factory('wikiService', function($http) {
 
     var wikiService = {
-        get: function(country) {
-            return $http.jsonp('http://es.wikipedia.org/w/api.php?titles=' + country.name.toLowerCase() + '&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK');
+        // get: function(country) {
+        //     return $http.jsonp('http://es.wikipedia.org/w/api.php?titles=' + country.name.toLowerCase() + '&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK');
+        get: function(individual) {
+          return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Ccategories%7Cimages%7Ctemplates&titles=' + individual.name+ '&callback=JSON_CALLBACK');
+
         }
     };
 
